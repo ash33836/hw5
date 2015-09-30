423ff55953eaffc5d8a4e0effa9c9a7d0ddca4c5
MacBook Owner <macbookowner@MacBooks-MacBook.local>
2015-09-29 20:23:35 -0400

    first commit

---
+++ js/cheatsheet.js
@@ -0,0 +1,38 @@
+
+//pop up alert
+alert("javascript cheatsheet");
+console.log("javascript cheatsheet");
+
+
+
+
+var names= ["Ashley", "Nicole", "Laura", "Mary"];
+console.log(names[3]);
+
+
+
+
+for (var i=0; i<7; i++) {
+	console.log (i);
+}
+
+
+function addAge (firstAge, secondAge) {
+console.log (firstAge + secondAge);
+}
+addAge(5,11);
+
+
+var boxWidth = 100;  
+var boxHeight= 10;  
+var boxArea = boxWidth * boxHeight;
+var idealBoxArea=2000
+console.log(boxArea);
+
+if (boxArea === idealBoxArea) 
+	{console.log ("Perfect Box Area");}
+else { console.log ("Need A New Box");}
+
+
+
+
